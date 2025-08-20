17fb9f26ea2c84552e07b75fbc09affcd285e458
ryk2025 <ruyo.ko@rightcode.co.jp>
2025-08-20 19:36:35 +0900

    spring bootベース

---
+++ inventory-bk/src/main/java/inventory/example/inventory_id/InventoryIdApplication.java
@@ -0,0 +1,13 @@
+package inventory.example.inventory_id;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryIdApplication.class, args);
+	}
+
+}
